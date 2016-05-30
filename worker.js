ba0150951befabb6b28297bcbef91b5253f4c510
alexey-ernest <alexey.ernest@gmail.com>
2016-05-30 19:28:44 +0300

    Fragment encoding functionality

--- worker.js
+++ worker.js
@@ -19,66 +19,119 @@ function handleError(err) {
   if (err) console.error(err);
 }
 
-function publishSegmentEvent(video, uri, fn) {
-  var fileName = path.basename(uri);
-  var pattern = /_(\d+)/;
-  var match = pattern.exec(fileName);
-  var idx = parseInt(match[1]);
-
-  video.segment = {
-    index: idx,
-    uri: uri
-  };
+function publishSegmentEvent(video, segment, fn) {
+  video.segment = segment;
   bus.publishSegmentExtracted(video, fn);
+}
+
+function publishSegmentEncodedEvent(video, segmentOutputs, fn) {
+  var segmentOutput = video.segment;
+  segmentOutput.outputs = segmentOutputs;
+
+  delete video.segment;
+  video.segmentOutputs = [segmentOutput];
+  bus.publishSegmentProcessed(video, fn);
+}
+
+function splitVideo(msg) {
+  var video = JSON.parse(msg.body);
+  debug('New video: ' + JSON.stringify(video));
+
+  var shiva = new Shiva();
+  shiva
+    .on('segment', function (segment) {
+      // segment extracted
+      publishSegmentEvent(video, segment, handleError);
+    })
+    .on('error', function(err) {
+      handleError(err);
+    });
+
+  // split video to segments
+  shiva.extractSegmentsToS3(video.uri, bucket, function (err) {
+    if (err) {
+      if (err.fatal) {
+        handleError(err);
+      } else {
+        // re-queue the message again if not fatal
+        debug('Video segmenting failed for ' + video.uri +  ', skipping the file: ' + err);
+      }
+      return;
+    }
+
+    debug('Video segmenting completed successfully for file ' + video.uri);
+    msg.finish();
+  });
+}
+
+function encodeSegment(msg) {
+  var videoSegment = JSON.parse(msg.body);
+  debug('Video ' + videoSegment.id + ' segment received for encoding: ' + JSON.stringify(videoSegment));
+
+  var segmentUri = videoSegment.segment.uri;
+
+  var formats = [];
+  var sizes = [];
+  videoSegment.outputs.forEach(function (o) {
+    if (formats.indexOf(o.format) === -1) {
+      formats.push(o.format);
+    }
+    if (sizes.indexOf(o.size) === -1) {
+      sizes.push(o.size);
+    }
+  });
+
+  var shiva = new Shiva();
+  shiva
+    .on('encoded', function (data) {
+      // segment extracted
+      publishSegmentEncodedEvent(videoSegment, data, handleError);
+    })
+    .on('error', function(err) {
+      handleError(err);
+    });
+
+  // encode segment
+  shiva.encodeAndUploadVideoToS3(segmentUri, formats, sizes, bucket, function (err) {
+    if (err) {
+      if (err.fatal) {
+        handleError(err);
+      } else {
+        // re-queue the message again if not fatal
+        debug('Video encoding failed for ' + segmentUri +  ', skipping the file: ' + err);
+      }
+      return;
+    }
+
+    debug('Video encoding completed successfully for file ' + segmentUri);
+    msg.finish();
+  });
+}
+
+function mergeSegment(msg) {
+  var video = JSON.parse(msg.body);
+  debug('Video ' + video.id + ' processed segments: ' + JSON.stringify(video));
+
+  // merge segment messages or concat segments into video if all processed
 }
 
 module.exports = function () {
   bus
     // Videos
     .on('video', function (msg) {
-      var video = JSON.parse(msg.body);
-      debug('New video: ' + JSON.stringify(video));
-
-      // split video to segments
-      var shiva = new Shiva();
-      shiva
-        .on('segment', function (uri) {
-          // generate segment event
-          publishSegmentEvent(video, uri, handleError);
-        })
-        .on('error', function(err) {
-          handleError(err);
-        });
-
-      shiva.extractSegmentsToS3(video.uri, bucket, function (err) {
-        if (err) {
-          if (err.fatal) {
-            handleError(err);
-          } else {
-            // re-queue the message again if not fatal
-            debug('Video segmenting failed for ' + video.uri +  ', skipping the file: ' + err);
-          }
-          return;
-        }
-
-        debug('Video segmenting completed successfully for file ' + video.uri);
-        msg.finish();
-      });
+      splitVideo(msg);
     })
 
     // Segments
     .on('segment', function (msg) {
-      var segment = JSON.parse(msg.body);
-      debug('Video ' + segment.id + ' segment: ' + JSON.stringify(segment));
-
-      // encode segment
+      encodeSegment(msg);
     })
+
+    // Processed Segments
     .on('processed-segments', function (msg) {
-      var segments = JSON.parse(msg.body);
-      debug('Video ' + segments.videoId + ' processed segments: ' + JSON.stringify(segments));
-
-      // merge segment messages or concat segments into video if all processed
+      mergeSegment(msg);
     })
+    
     .listen();
 
   debug('Listening for messages...');
